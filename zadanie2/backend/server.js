97b140dafd2c8217503ac7c880c5c2dd069c8a6e
bananky <wojnarskap@gmail.com>
2025-06-09 22:07:53 +0200

    commit5

--- zadanie2/backend/server.js
+++ zadanie2/backend/server.js
@@ -5,23 +5,19 @@ const { sequelize } = require('./models');
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
+app.use('/api/products', require('./routes/products'));
+app.use('/api/categories', require('./routes/categories'));
 
-
-const productRoutes = require('./routes/products');
-const categoryRoutes = require('./routes/categories');
-
-app.use('/api/products', productRoutes);
-app.use('/api/categories', categoryRoutes);
-
-sequelize.sync()
-  .then(() => {
-    console.log("Baza danych połączona.");
-    app.listen(PORT, () => {
-      console.log(`API running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Błąd połączenia z bazą:", err);
+sequelize.sync().then(() => {
+  console.log("Baza danych połączona.");
+  app.listen(PORT, () => {
+    console.log(`API running on http://localhost:${PORT}`);
   });
+});
