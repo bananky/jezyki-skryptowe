72075e4c46386a21bb5f62526a64c4cb4093d870
bananky <wojnarskap@gmail.com>
2025-06-26 16:36:17 +0200

    commit3

--- zadanie5/actions/actions.py
+++ zadanie5/actions/actions.py
@@ -23,3 +23,24 @@ class ActionOpeningHours(Action):
 
         dispatcher.utter_message(text=hours)
         return []
+        
+class ActionMenu(Action):
+    def name(self) -> str:
+        return "action_menu"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: dict) -> list:
+
+        filepath = os.path.join(os.path.dirname(__file__), "menu.json")
+
+        try:
+            with open(filepath, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                menu_items = data.get("menu", [])
+                menu_text = "\n".join(f"- {item}" for item in menu_items)
+                dispatcher.utter_message(text=f"Oto dzisiejsze menu:\n{menu_text}")
+        except Exception as e:
+            dispatcher.utter_message(text="❌ Nie udało się załadować menu.")
+
+        return []
